278eaf694d729abf0a54382787e67e334139432a
VishSeran <vishwa.sovis@gmail.com>
2025-11-23 20:52:28 +0530

    HomePage.tsx updated

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactPlayer from "react-player";
 
 function HomePage(){
     
@@ -7,6 +8,7 @@ function HomePage(){
     return(
         <div>
             <h1>Hello, welcome user!</h1>
+            <ReactPlayer url="https://youtu.be/Pb9n23ILj-Y?si=fYjUKMvr_f38L4Gl" controls playing={false} volume={0.5}/>
         </div>
     );
 }
